f06ebf58ec78ac89d1a58d730b676b5a301a8d9c
LeLuongg <161265807+LeLuongg@users.noreply.github.com>
2024-12-25 01:11:43 +0700

    update routes

--- sell-management/routes/web.php
+++ sell-management/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\OrderDetailController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::resource('products', ProductController::class);
 Route::resource('orders', OrdersController::class);
+Route::resource('orders.order_details', OrderDetailController::class);
