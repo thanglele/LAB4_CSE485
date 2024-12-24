b964a97e1a3c57bad05531764051b86373a24916
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-24 21:20:22 +0700

    Update BookController.php

--- library-management/app/Http/Controllers/BookController.php
+++ library-management/app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
@@ -11,7 +12,8 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+        $books = Book::all();
+        return view('books.index', compact('books'));
     }
 
     /**
