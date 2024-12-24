74c63d0d2538268b6199162890943ffee7553761
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-24 08:32:09 +0700

    Update web.php

--- library-management/routes/web.php
+++ library-management/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReaderController;
+use App\Http\Controllers\BorrowController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('readers', ReaderController::class);
+Route::resource('borrows', BorrowController::class);
