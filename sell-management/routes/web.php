ebc6559297938b63871e95f8aafe587328f427ab
AnhNgo127 <161997871+AnhNgo127@users.noreply.github.com>
2024-12-25 22:44:49 +0700

    route hiển thị

--- sell-management/routes/web.php
+++ sell-management/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->name('dashboard');
+Route::resource('products', ProductController::class);
+Route::resource('customers', CustomerController::class);
+Route::resource('orders', OrderController::class);
