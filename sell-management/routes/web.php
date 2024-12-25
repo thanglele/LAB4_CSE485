64366ad5290588d96b69fdecac9ab7f0f4615aed
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-25 23:07:56 +0700

    Update web.php

--- sell-management/routes/web.php
+++ sell-management/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('products', ProductController::class);
+Route::resource('customers', CustomerController::class);
