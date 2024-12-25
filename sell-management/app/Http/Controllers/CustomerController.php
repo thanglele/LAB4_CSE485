c819ae5708cbe76a1c4a5829d0bb0aa1eebf094e
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-25 23:06:31 +0700

    Create CustomerController.php

---
+++ sell-management/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+        return view('customers.index', compact('customers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('customers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required|digits:10',
+            'email' => 'required|email|unique:customers,email',
+        ]);
+
+        Customer::create($request->all());
+        return redirect()->route('customers.index')->with('success', 'Khách hàng được thêm thành công!');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $customer = Customer::findOrFail($id);
+        return view('customers.edit', compact('customer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required|digits:10',
+            'email' => 'required|email|unique:customers,email,' . $id,
+        ]);
+
+        $customer = Customer::findOrFail($id);
+        $customer->update($request->all());
+        return redirect()->route('customers.index')->with('success', 'Thông tin khách hàng được cập nhật thành công!');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+        return redirect()->route('customers.index')->with('success', 'Khách hàng đã bị xóa!');
+
+    }
+}
