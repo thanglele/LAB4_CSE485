3ea5c3dfa033aa8df520c751e7c1a5de5766ba9d
Bùi Viết Hiển <hienhachyn102@gmail.com>
2024-12-25 23:00:07 +0700

    Update 2024_12_23_214224_customers.php

--- sell-management/database/migrations/2024_12_23_214224_customers.php
+++ sell-management/database/migrations/2024_12_23_214224_customers.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string("name");
             $table->string("address");
             $table->string("phone");
-            $table->string("email");
+            $table->string('email')->unique();
             $table->dateTime("updated_at");
             $table->dateTime("created_at");
         });
